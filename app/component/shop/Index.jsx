5d6c875f5c0b65c6475f2b0d22fd9cc932c29917
1906526613@qq.com <1906526613@qq.com>
2018-03-21 01:06:29 +0800

    渲染list+处理item

--- app/component/shop/Index.jsx
+++ app/component/shop/Index.jsx
@@ -1,45 +1,44 @@
 import React from 'react';
 import "../../public/css/shop.pcss";
-import Seconds from '../common/Seconds'
 
-const Index = () => <Seconds title='商城'/>
-// class Index extends React.Component {
-//     constructor(props){
-//         super(props);
-//         this.state = { seconds : 0 }
-//     }
+class Index extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            list:[
+                {id:1,title:"HOUSTON ROCKETS-01"},
+                {id:2,title:"HOUSTON ROCKETS-02"},
+                {id:3,title:"HOUSTON ROCKETS-03"},
+                {id:4,title:"HOUSTON ROCKETS-04"},
+                {id:5,title:"HOUSTON ROCKETS-05"},
 
-//     tick(){
-//         this.setState( prevState =>({
-//             seconds:prevState.seconds + 1
-//         }))
-//     }
+                {id:6,title:"HOUSTON ROCKETS-06"},
+                {id:7,title:"HOUSTON ROCKETS-07"},
+                {id:8,title:"HOUSTON ROCKETS-08"},
+                {id:9,title:"HOUSTON ROCKETS-10"}
+            ]
+        }
+    }
+    handleItemDel(id){
+        console.log(id)
+        let list = this.state.list;
+        list.splice(list.findIndex(data =>data.id ==id),1)
+        this.setState({list:list})
+    }
+    render(){
+        let {list} = this.state;
+        return(
+            <div className='content'>
+                  {
+                     list.map(data=>
+                             <li key={data.id}>{data.title} <button onClick = {this.handleItemDel.bind(this,data.id)}>删除</button></li>
+                      )
+                  }
+            </div>
+        )
+    } 
+}
 
-//     componentDidMount(){
-//         this.interval = setInterval( ()=>{
-//             this.tick()
-//         },1000)
-//     }
-
-//     componentWillUnmount(){
-//         clearInterval(this.interval)
-//     }
-
-//     render(){
-//         return(
-//             <div className="cont">
-//                <div className="top">
-//                    <i className="bd_logo1"></i>
-//                    这是商城,哈哈
-//                </div>
-//                 <div className="bottom">
-//                       seconds：{this.state.seconds}
-//                 </div>
-              
-//             </div>
-//         )
-//     }
-// }
 
 export default Index;
 
